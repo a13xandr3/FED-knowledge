62c94b53a71bdd0edbc717e63e779a10969f9d32
a13xandr3 <estevesalves@icloud.com>
2026-06-19 23:28:56 -0300

    ajustes configurações

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,23 +1,22 @@
-const { createCjsPreset } = require('jest-preset-angular/presets');
-
 /** @type {import('jest').Config} */
 module.exports = {
-  // carrega transform, testEnvironment, etc, próprio para Angular
-  ...createCjsPreset(),
-
-  // aponta para o setup que inicializa Zone + Angular TestBed
+  preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-
-  // opcional, mas deixa explícito
   testEnvironment: 'jsdom',
-
-  // garante que só pegue specs do src e não o antigo test.ts do Karma
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
-
-  // para evitar conflito com o antigo test runner do Angular
-  testPathIgnorePatterns: ['<rootDir>/src/test.ts'],
-
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/coverage/',
+    '<rootDir>/src/test.ts',
+  ],
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
   },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/**/*.d.ts',
+    '!src/environments/**',
+  ],
 };
