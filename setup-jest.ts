f1ed3b5154754a994d058c6bf7e75097aafc117d
a13xandr3 <estevesalves@icloud.com>
2026-06-20 00:07:36 -0300

    mudança de versão

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,9 +1,20 @@
 import 'jest-preset-angular/setup-jest';
 
-declare const require: (moduleName: string) => { webcrypto: Crypto };
+declare const require: (moduleName: string) => any;
 const { webcrypto } = require('crypto');
+const { TextDecoder, TextEncoder } = require('util');
 
 Object.defineProperty(globalThis, 'crypto', {
   value: webcrypto,
+  configurable: true,
+});
+
+Object.defineProperty(globalThis, 'TextDecoder', {
+  value: TextDecoder,
+  configurable: true,
+});
+
+Object.defineProperty(globalThis, 'TextEncoder', {
+  value: TextEncoder,
   configurable: true,
 });
